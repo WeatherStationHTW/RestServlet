6059514afcc8dfc6d9e1e8f73db735a1ac147c7c
HTWWeatherStation <hendrikkiedrowski@gmail.com>
2015-05-13 13:39:06 +0200

    Klasse für Datenstruktur (1.Entwurf)

---
+++ RestFul/Wetter/WeatherData.java
@@ -0,0 +1,20 @@
+import java.time.LocalTime;
+
+
+public class WeatherData {
+	
+	private String stationid;
+	private Double stationlong;
+	private Double stationlat;
+	private Integer sensorid;
+	private String sensortype;
+	private LocalTime timestamp;
+	private Double value;
+	private String unit;
+	
+
+	public WeatherData() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
