88fa8620a378e944179b420c7aef7560144b699d
HTWWeatherStation <hendrikkiedrowski@gmail.com>
2015-07-06 14:28:03 +0200

    Korrektur dbcon pfad der Tabelle

--- RestFul/Wetter/dbconn.java
+++ RestFul/Wetter/dbconn.java
@@ -15,11 +15,12 @@ public class dbconn {
 			String result = "OK";
 			try
 			{
+				
 				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Wetterstation1", "root", "");
 			    stat = con.createStatement();
 				rs = null;
-				stat.execute("INSERT INTO `Wetterstation1`.`Wetterstation` (`sensorid`, `sensortype`, `stationid`, `stationlat`, `stationlong`, `timestamp`, `unit`, `value`) "
-																 + "VALUES (record.getSensorid(),record.getSensortype(),record.getStationid(),record.getStationLat(),record.getStationlong(),record.getTimestamp(),record.getUnit(),record.getValue())");
+				stat.execute("INSERT INTO `Wetterstation`.`WeatherData` (`stationid`, `stationlong`, `stationlat`, `sensorid`, `sensortype`, `timestamp`, `value`, `unit`)"
+															  + "VALUES (record.getSensorid(),record.getSensortype(),record.getStationid(),record.getStationLat(),record.getStationlong(),record.getTimestamp(),record.getUnit(),record.getValue())");
 				return result;
 			}
 			
